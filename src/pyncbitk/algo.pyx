6b41552aac195416a324107510c36f6a52dd014e
Martin Larralde <martin.larralde@embl.de>
2024-07-30 20:18:36 +0200

    Expose `Blast` options and add subclass for `blastn` task

--- src/pyncbitk/algo.pyx
+++ src/pyncbitk/algo.pyx
@@ -6,10 +6,12 @@ from libcpp.string cimport string
 from libcpp.utility cimport move
 
 from .toolkit.algo.blast.api.bl2seq cimport CBl2Seq
-from .toolkit.algo.blast.api.blast_types cimport EProgram, ProgramNameToEnum, TSeqAlignVector
+from .toolkit.algo.blast.api.blast_types cimport EProgram, ProgramNameToEnum, TSeqAlignVector, EProgramToTaskName
 from .toolkit.algo.blast.api.sseqloc cimport SSeqLoc, TSeqLocVector
 from .toolkit.algo.blast.api.local_blast cimport CLocalBlast
 from .toolkit.algo.blast.api.blast_options_handle cimport CBlastOptionsHandle, CBlastOptionsFactory
+from .toolkit.algo.blast.api.blast_options_handle cimport CBlastOptionsHandle, CBlastOptionsFactory
+from .toolkit.algo.blast.api.blast_nucl_options cimport CBlastNucleotideOptionsHandle
 from .toolkit.algo.blast.api.query_data cimport IQueryFactory
 from .toolkit.algo.blast.api.objmgr_query_data cimport CObjMgr_QueryFactory
 from .toolkit.algo.blast.api.local_db_adapter cimport CLocalDbAdapter
@@ -94,12 +96,12 @@ cdef class SearchResultsSet:
         if _index < 0:
             _index += _length
         if _index < 0 or _index >= _length:
-            raise IndexError(index) 
+            raise IndexError(index)
 
         cdef CSearchResultSet* obj  = &self._ref.GetObject()
-        cdef CSearchResults*   item = &obj[0][<CSearchResults_size_type> _index] 
+        cdef CSearchResults*   item = &obj[0][<CSearchResults_size_type> _index]
         return SearchResults._wrap(CRef[CSearchResults](item))
-        
+
 
 cdef class SearchResults:
     cdef CRef[CSearchResults] _ref
@@ -120,27 +122,138 @@ cdef class SearchResults:
 cdef class Blast:
     cdef CRef[CBlastOptionsHandle] _opt
 
-    def __init__(self, str program):
-        cdef EProgram p
-        cdef CBlastOptionsHandle* opt 
-        cdef bytes    _program = program.encode()
+    @staticmethod
+    def tasks():
+        return [ x.decode() for x in CBlastOptionsFactory.GetTasks() ]
 
-        try:
-            p = ProgramNameToEnum(_program)
-        except Exception as e:
-            raise ValueError(f"Invalid BLAST program: {program!r}") from e
+    def __init__(self):
+        raise TypeError("Cannot instantiate abstract class Blast")
 
-        opt = CBlastOptionsFactory.Create(p)
-        self._opt.Reset(opt)
+    # def __init__(
+    #     self,
+    #     str program,
+    #     *,
+    #     int window_size = 40,
+    # ):
+    #     cdef EProgram p
+    #     cdef CBlastOptionsHandle* opt
+    #     cdef bytes    _program = program.encode()
 
-    def run(self, queries, subjects, bool scan_mode = False):
-        
-        cdef TSeqLocVector         _queries  
+    #     try:
+    #         p = ProgramNameToEnum(_program)
+    #     except Exception as e:
+    #         raise ValueError(f"Invalid BLAST program: {program!r}") from e
+
+    #     opt = CBlastOptionsFactory.Create(p)
+    #     self._opt.Reset(opt)
+
+    def __repr__(self):
+        cdef str ty = self.__class__.__name__
+        return f"{ty}({self.program!r}, window_size={self.window_size})"
+
+    # --- Properties -----------------------------------------------------------
+
+    @property
+    def program(self):
+        cdef EProgram program = self._opt.GetObject().GetOptions().GetProgram()
+        return EProgramToTaskName(program).decode('ascii')
+
+    @property
+    def window_size(self):
+        """`int`: The window size for multiple hits.
+        """
+        return self._opt.GetObject().GetWindowSize()
+
+    @window_size.setter
+    def window_size(self, int window_size):
+        if window_size < 0:
+            raise ValueError(f"window_size must be a positive integer, got {window_size!r}")
+        self._opt.GetObject().SetWindowSize(window_size)
+
+    @property
+    def off_diagonal_range(self):
+        """`int`: The number of off-diagonals to search for the second hit.
+        """
+        return self._opt.GetObject().GetOffDiagonalRange()
+
+    @property
+    def xdrop_gap(self):
+        """`float`: X-dropoff value (in bits) for preliminary gapped extensions.
+        """
+        return self._opt.GetObject().GetGapXDropoff()
+
+    @property
+    def xdrop_gap_final(self):
+        """`float`: X-dropoff value (in bits) for final gapped alignment.
+        """
+        return self._opt.GetObject().GetGapXDropoffFinal()
+
+    # @property
+    # def max_hsps(self):
+    #     """`int`: Maximum number of HSPs per subject to save for each query.
+    #     """
+    #     return self._opt.GetObject().GetMaxHspsPerSubject()
+
+    @property
+    def evalue(self):
+        """`float`: Expectation value (E) threshold for saving hits.
+        """
+        return self._opt.GetObject().GetEvalueThreshold()
+
+    @property
+    def percent_identity(self):
+        """`float`: Percentage identity threshold for saving hits.
+        """
+        return self._opt.GetObject().GetPercentIdentity()
+
+    @property
+    def coverage_hsp(self):
+        """`float`: Query coverage percentage per HSP.
+        """
+        return self._opt.GetObject().GetQueryCovHspPerc()
+
+    @property
+    def gapped(self):
+        """`bool`: `False` if alignments are performed in ungapped mode only.
+        """
+        return self._opt.GetObject().GetGappedMode()
+
+    @property
+    def culling_limit(self):
+        """`int`: The culling limit for hits.
+
+        If the query range of a hit is enveloped by that of at least this many
+        higher-scoring hits, delete the hit.
+
+        """
+        return self._opt.GetObject().GetCullingLimit()
+
+    @property
+    def db_size(self):
+        """`int`: The effective length of the database.
+        """
+        return self._opt.GetObject().GetDbLength()
+
+    @property
+    def search_space(self):
+        """`int`: The effective length of the search space.
+        """
+        return self._opt.GetObject().GetEffectiveSearchSpace()
+
+    # --- Public Methods -------------------------------------------------------
+
+    cpdef SearchResultsSet run(
+        self,
+        queries,
+        subjects,
+        bool scan_mode = False
+    ):
+        cdef TSeqLocVector         _queries
         cdef TSeqLocVector         _subjects
         cdef BlastSeqLoc           seqloc
-        cdef CRef[IQueryFactory]   query_factory      
-        cdef CRef[IQueryFactory]   subject_factory    
-        cdef CRef[CLocalDbAdapter] db             
+        cdef CRef[IQueryFactory]   query_factory
+        cdef CRef[IQueryFactory]   subject_factory
+        cdef CRef[CLocalDbAdapter] db
         cdef CRef[CLocalBlast]     blast
 
         if isinstance(queries, BlastSeqLoc):
@@ -160,12 +273,34 @@ cdef class Blast:
         # if (m_InterruptFnx != NULL) {
         #     m_Blast->SetInterruptCallback(m_InterruptFnx, m_InterruptUserData);
         # }
-        # // Set the hitlist size to the total number of subject sequences, to 
+        # // Set the hitlist size to the total number of subject sequences, to
         # // make sure that no hits are discarded (ported from CBl2Seq::SetupSearch
-        # m_OptsHandle.SetHitlistSize((int) m_tSubjects.size()); 
+        # m_OptsHandle.SetHitlistSize((int) m_tSubjects.size());
 
         results = blast.GetObject().Run()
         return SearchResultsSet._wrap(results)
         # messages = blast.GetSearchMessages() # TODO
+
+
+cdef class BlastN(Blast):
+    cdef CRef[CBlastNucleotideOptionsHandle] _blastn_opt
+
+    def __init__(self):
+
+        cdef CBlastNucleotideOptionsHandle* handle = new CBlastNucleotideOptionsHandle()
+        handle.SetTraditionalBlastnDefaults()
+
+        cdef CBlastOptionsHandle* base = &(handle[0])
+
+        self._blastn_opt.Reset(handle)
+        self._opt.Reset(<CBlastOptionsHandle*> handle)
+
+
+
+cdef class ProteinBlast(Blast):
+    pass
+
+cdef class MappingBlast(Blast):
+    pass
 
 
