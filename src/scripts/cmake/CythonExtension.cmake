c47720841397b70b816f6f35a17e32e81bbebdf7
Martin Larralde <martin.larralde@embl.de>
2024-08-15 01:27:10 +0200

    Fix vendor install of built NCBI libraries in `CMakeLists.txt`

--- src/scripts/cmake/CythonExtension.cmake
+++ src/scripts/cmake/CythonExtension.cmake
@@ -24,11 +24,12 @@ else()
 endif()
 
 macro(cython_extension _name)
-
+  # Make sure that the source directory is known
   if(NOT DEFINED PYTHON_EXTENSIONS_SOURCE_DIR)
     message(FATAL_ERROR "The PYTHON_EXTENSIONS_SOURCE_DIR variable has not been set.")
   endif()
 
+  # Generate C++ file from Cython file
   add_custom_command(
     OUTPUT ${_name}.cpp
     COMMENT
@@ -42,14 +43,33 @@ macro(cython_extension _name)
     MAIN_DEPENDENCY 
       ${_name}.pyx
     VERBATIM)
+  
+  # Add Python library target
+  set(_target "py_${_name}")
+  python_add_library(${_target} MODULE WITH_SOABI ${_name}.pyx ${_name}.cpp)
+  set_target_properties(${_target} PROPERTIES OUTPUT_NAME ${_name} )
 
-  python_add_library("py_${_name}" MODULE WITH_SOABI ${_name}.pyx ${_name}.cpp)
-  set_target_properties( "py_${_name}" PROPERTIES OUTPUT_NAME  ${_name} )
-
+  # Link to required C++ Toolkit libraries
   foreach(_lib IN LISTS PYNCBITK_NCBI_LIBS)
-    target_link_libraries("py_${_name}" PUBLIC ${_lib})
+    target_link_libraries(${_target} PUBLIC $<TARGET_LINKER_FILE:${_lib}>)
   endforeach()
 
+  # Preserve the relative project structure in the install directory
   string(REGEX REPLACE "^${PYTHON_EXTENSIONS_SOURCE_DIR}/?" "" _dest_folder ${CMAKE_CURRENT_SOURCE_DIR})
-  install(TARGETS "py_${_name}" DESTINATION ${_dest_folder} )
+  install(TARGETS ${_target} DESTINATION ${_dest_folder} )
+  message(DEBUG "Install folder for extension ${_name}: ${_dest_folder}")
+
+  # Patch the RPATH to the installed libs (only if libs are installed locally)
+  if(DEFINED PYTHON_LIBS_INSTALL_DIR)
+    cmake_path(SET _path NORMALIZE ${_dest_folder})
+    string(REPLACE "/" ";" _components ${_path})
+    set(_rpath "\$ORIGIN/")
+    foreach(_x IN LISTS _components)
+      string(APPEND _rpath "../")
+    endforeach()
+    string(APPEND _rpath "${PYTHON_LIBS_INSTALL_DIR}")
+    set_target_properties(${_target} PROPERTIES INSTALL_RPATH ${_rpath})
+    message(DEBUG "RPATH for extension ${_name}: ${_rpath}")
+  endif()
+
 endmacro()
