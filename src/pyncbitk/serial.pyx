a827c29f8edc7e86032a7f19926649d7fa5b3441
Martin Larralde <martin.larralde@embl.de>
2025-10-30 14:58:01 +0100

    Implement `__hash__` for `Serial` objects based on `CSerialObject` ASN serialization

--- src/pyncbitk/serial.pyx
+++ src/pyncbitk/serial.pyx
@@ -1,7 +1,8 @@
 # cython: language_level=3, linetrace=True, binding=True
 
 from libcpp.string cimport string
-from iostream cimport ostream, stringbuf
+from libcpp.cast cimport static_cast
+from iostream cimport ostream, stringbuf, streambuf
 
 from .toolkit.corelib.ncbistre cimport CNcbiOstream
 from .toolkit.serial.serialbase cimport CSerialObject, MSerial_Format, MSerial_Flags, TSerial_Format_Flags
@@ -18,6 +19,50 @@ cdef extern from * nogil:
     }
     """
     void DumpObject(CNcbiOstream& os, CSerialObject& obj, MSerial_Format& fmt) except +
+
+
+# NOTE: This is a custom `streambuf` that hashes its input using FNV-64 instead 
+#       of writing it to a buffer; this allows using the CSerialObject code which 
+#       is only implemented for object serialization and use the serialized 
+#       representation of the object to hash it on-the-fly.
+cdef extern from * nogil:
+    """
+    class hashbuf : public std::basic_streambuf< char, std::char_traits< char > >
+    {
+    private:
+        class hasher
+        {
+        private:
+            const size_t PRIME = 0x00000100000001b3;  // FNV-64 prime
+            const size_t BASIS = 0xcbf29ce484222325;
+        public:
+            inline hasher() : state(BASIS) {}
+            void operator<< (char c) { state ^= c; state *= PRIME; }
+            size_t finish() { return state; }
+        private:
+            size_t state;
+        };
+
+        hasher h;
+
+    public:
+        inline hashbuf() :
+        std::basic_streambuf< char, std::char_traits< char > >(),
+        h(hasher())
+        {}
+
+        size_t finish() { return h.finish(); }
+        std::streamsize xsputn(const char* c, std::streamsize n) override
+        {
+            for(std::streamsize i = 0; i < n; i++) h << c[i];
+            return n;
+        }
+    };
+    """
+    cppclass hashbuf(streambuf):
+        hashbuf()
+        size_t finish() noexcept
+
 
 cdef dict _SERIAL_DATAFORMAT_STR = {
     ESerialDataFormat.eSerial_None: None,
@@ -39,7 +84,7 @@ cdef class Serial:
         return NULL
 
     def __eq__(self, object other):
-        cdef CSerialObject* left
+        cdef CSerialObject* lostreameft
         cdef CSerialObject* right
 
         if not isinstance(other, Serial):
@@ -52,6 +97,34 @@ cdef class Serial:
             return left.Equals(right[0])
         except:
             return NotImplemented
+
+    def __hash__(self):
+        cdef ESerialDataFormat    sdf
+        cdef MSerial_Format*      fmt
+        cdef string               out
+        cdef CNcbiOstream*        s
+        cdef CSerialObject*       serial = self._serial()
+        cdef hashbuf*             hasher
+        cdef TSerial_Format_Flags flags  = 0
+
+        assert serial is not NULL
+
+        # use asn binary serialization as it is likely the most compact.
+        sdf = ESerialDataFormat.eSerial_AsnBinary
+        flags |= <TSerial_Format_Flags> ESerial_Xml_Flags.fSerial_Xml_NoIndentation
+        flags |= <TSerial_Format_Flags> ESerial_Xml_Flags.fSerial_Xml_NoEol
+
+        try:
+            hasher = new hashbuf()
+            s = new ostream(&hasher[0])
+            fmt = new MSerial_Format(sdf, flags)
+            with nogil:
+                DumpObject(s[0], serial[0], fmt[0])
+            return hasher.finish()
+        finally:
+            del s
+            del fmt
+            del hasher
 
     cpdef string dumps(
         self,
